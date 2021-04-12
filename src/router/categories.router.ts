902bf291a0854239c429dc1f8e8fc42fcc1ddb16
ggoleta <rogerio_contato@hotmail.com>
2021-04-12 16:06:56 -0300

    create class CreateCategoryService.ts

--- src/router/categories.router.ts
+++ src/router/categories.router.ts
@@ -1,19 +1,15 @@
 import { Router } from "express";
 
 import CategoriesRepository from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRouter = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRouter.post("/", (request, response) => {
   const { name, description } = request.body;
-  const category = categoriesRepository.findByName(name);
-
-  if (category) {
-    return response.status(400).json({ error: "Category already exists!" });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
   return response.status(201).send();
 });
 
