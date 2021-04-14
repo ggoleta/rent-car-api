bb837df4af7816f969e5e46649fb856285c93f4f
ggoleta <rogerio_contato@hotmail.com>
2021-04-14 09:45:04 -0300

    creating singleton to CategoriesRepository

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -6,15 +6,24 @@ import {
 
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
-  constructor() {
+
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
     Object.assign(category, { name, description, created_at: new Date() });
     this.categories.push(category);
-    console.log(this.categories);
   }
 
   list(): Category[] {
