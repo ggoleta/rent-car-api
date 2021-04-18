80bde6153e9de0c669fd4c24a9abeed30cbe4fe5
ggoleta <rogerio_contato@hotmail.com>
2021-04-18 15:57:34 -0300

    creating swagger install & json

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./router";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
