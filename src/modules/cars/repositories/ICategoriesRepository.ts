a5bc59bf5c24b671888e52ed8ae264a04a0803ab
ggoleta <rogerio_contato@hotmail.com>
2021-04-23 16:28:43 -0300

    create docker, docker-compose and save DB

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../model/Category";
+import { Category } from "../entities/Category";
 
 interface ICreateCategoryDTO {
   name: string;
@@ -6,9 +6,9 @@ interface ICreateCategoryDTO {
 }
 
 interface ICategoriesRepository {
-  findByName(name: string): Category | undefined;
-  create({ name, description }: ICreateCategoryDTO): void;
-  list(): Category[];
+  findByName(name: string): Promise<Category | undefined>;
+  create({ name, description }: ICreateCategoryDTO): Promise<void>;
+  list(): Promise<Category[]>;
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO };
--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -7,8 +7,8 @@ interface IRequest {
 
 class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
-  execute({ name, description }: IRequest): void {
-    const category = this.categoriesRepository.findByName(name);
+  async execute({ name, description }: IRequest): Promise<void> {
+    const category = await this.categoriesRepository.findByName(name);
 
     if (category) {
       throw new Error("Category already exists!");
